5d5268d9d00108679aacf3afae3867d3f7e1300e
Noble Mutoko <noblemutoko@gmail.com>
2022-07-26 12:12:52 +0000

    print lowercase characters[a-z]y

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: Resultof characters
+*/
+
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+
+return (0);
+}
